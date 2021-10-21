3079b583bd37cb7f7af5c77219bea40322ca9f30
Buciuman Adrian Petrisor <adieuadi@gmail.com>
2021-10-21 17:16:40 +0300

    Added * operation

--- parser.cpp
+++ parser.cpp
@@ -181,16 +181,16 @@ std::shared_ptr<Expr> Parser::ParseCallExpr()
 // -----------------------------------------------------------------------------
 std::shared_ptr<Expr> Parser::ParseAddSubExpr()
 {
-  std::shared_ptr<Expr> term = ParseCallExpr();
+  std::shared_ptr<Expr> term = ParseProductExpr();
   while (Current().Is(Token::Kind::PLUS) || Current().Is(Token::Kind::MINUS)) {
   	if(Current().Is(Token::Kind::PLUS)) {
-  	    lexer_.Next();
-	    auto rhs = ParseCallExpr();
+      lexer_.Next();
+	    auto rhs = ParseProductExpr();
 	    term = std::make_shared<BinaryExpr>(BinaryExpr::Kind::ADD, term, rhs);
   	}
   	else {
-  	    lexer_.Next();
-	    auto rhs = ParseCallExpr();
+      lexer_.Next();
+	    auto rhs = ParseProductExpr();
 	    term = std::make_shared<BinaryExpr>(BinaryExpr::Kind::SUB, term, rhs);
   	}
     
